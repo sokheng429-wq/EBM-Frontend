b2a81ff60a1f22716b760c882398ba97717a8f19
sokheng429-wq <sokheng429@gmail.com>
2026-06-26 17:58:46 +0700

    initial commit

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,18 +1,19 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:8081/api",
+    baseURL: "https://ebm-backend-1.onrender.com/api",
     headers: {
         "Content-Type": "application/json",
     },
 });
 
-// 🔐 attach token automatically
 api.interceptors.request.use((config) => {
     const token = localStorage.getItem("token");
 
     if (token) {
-        config.headers.Authorization = token.startsWith("Basic ") ? token : `Bearer ${token}`;
+        config.headers.Authorization = token.startsWith("Basic ")
+            ? token
+            : `Bearer ${token}`;
     }
 
     return config;
