cb0bf35cd9cf5cf44434415c48ee8b1bf0cbdb50
sokheng429-wq <sokheng429@gmail.com>
2026-07-06 01:38:06 +0700

    second commit

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || "http://localhost:8081/api",
+    baseURL: import.meta.env.VITE_API_URL || "https://mdkelephant.fit/",
     headers: {
         "Content-Type": "application/json",
     },
