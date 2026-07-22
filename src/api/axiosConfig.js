7d4d7951cfc286ab9fdc2105861a5f9a0fd54c11
sokheng429-wq <sokheng429@gmail.com>
2026-07-22 20:40:41 +0700

    fix forgot password

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || "https://ebm-backend.onrender.com/api", // https://ebm-backend.onrender.com/api
+    baseURL: import.meta.env.VITE_API_URL || "http://localhost:8081/api", // https://ebm-backend.onrender.com/api
     headers: {
         "Content-Type": "application/json",
     },
